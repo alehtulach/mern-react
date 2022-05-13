5d812dfb759d9e48348315197d2568bccdbee519
ATulach <otulach@gmail.com>
2022-05-13 17:11:13 +0400

    handle refresh token

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://localhost:3500" });
+const BASE_URL = "http://localhost:3500";
+
+export default axios.create({ baseURL: BASE_URL });
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
